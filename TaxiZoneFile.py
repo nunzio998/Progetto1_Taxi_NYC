a2699a3d3862e892763b45e1cd152c8ea4b86ac1
francesco <francesco.pinsone@alcampus.it>
2022-12-07 11:12:25 +0100

    refactor & adjustments in TaxiZoneFile.py

--- TaxiZoneFile.py
+++ TaxiZoneFile.py
@@ -1,23 +1,27 @@
 import pandas as pd
 import numpy as np
 
+
 class TaxiZoneFile:
 
     def __init__(self, file):
-        self.file = pd.read_csv(file)
+        self.dataFrame = pd.read_csv(file)
 
     def getDataFrame(self) -> pd.DataFrame:
         """
         Ritorna il dataframe relativo al file passato al costruttore della classe
         :return: DataFrame of the input file
         """
-        return self.file
+        return self.checkDataFrame(self.dataFrame)
 
-    def checkDataFrame(self, dataFrame) -> bool:
+    @classmethod
+    def checkDataFrame(cls, inputDataFrame) -> bool:
         """
         Effettua il check del data frame in input con conseguente
-        gestione di valori e della loro coerenza
-        :param dataFrame:
+        gestione di valori e della loro coerenza.
+        Si decide di eliminare eventuali valori NaN o Unknown.
+        :param cls:
+        :param inputDataFrame:
         :return:
         """
-        return True
+        return inputDataFrame
