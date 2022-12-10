bb2b966bc37df145c38e6d2a234616832095a2a8
Matteo Gardusi <matteo.gardusi@gmail.com>
2022-12-10 16:33:20 +0100

    FIX Minor develop

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-import DataAnalyses
-from TaxiTripFile import *
+import pandas as pd
+
 from DataAnalyses import *
-import streamlit as st
 import time
-from calendar import monthrange
 
 if __name__ == '__main__':
+
     start = time.perf_counter()
 
     # anno e mese selezionato
-    year = '2022'
-    month = '01'
+    year = '2020'
+    month = '10'
+    append_dataFrames = []
+    for month in ['01', '02', '03', '04', '05', '06', '07']:
+        zoneFilePath = "data/zone/taxi+_zone_lookup.csv"
+        tripFilePath = f"data/trip/{year}/yellow_tripdata_{year}-{month}.parquet"
 
-    zoneFilePath = "data/taxi+_zone_lookup.csv"
-    tripFilePath = f"data/yellow_tripdata_{year}-{month}.parquet"
+        # Check TripDataframe con partenza e arrivi in Zonefile (Gennaio-2022)
+        dataSet_Trip = DataAnalyses(zoneFilePath, tripFilePath, year, month)
 
-    # Check TripDataframe con partenza e arrivi in Zonefile (Gennaio-2022)
-    dataSet_Trip = DataAnalyses(zoneFilePath, tripFilePath, year, month)
+        dfTrip = dataSet_Trip.getBoroughAverageDataFrame()
 
-    print(dataSet_Trip.getAverageDataFrame())
+        append_dataFrames.append(dfTrip)
+
+    dfResult = pd.concat(append_dataFrames, ignore_index=True)
+
+    dfResult.to_csv('results/data.csv')
+
+    print(dfResult)
 
     end = time.perf_counter()
     print(f"Tempo di esecuzione: {round(end - start, 3)}")
