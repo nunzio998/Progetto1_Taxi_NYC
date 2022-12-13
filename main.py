f611b323074b953e513bc79af6fab4bed40856bd
Gabriele Giordano <giordano.gabriele@outlook.it>
2022-12-13 11:51:44 +0100

    gitignore update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from FileMaker.AverageFileMaker import AverageFileMaker
 
 if __name__ == '__main__':
-    analisi = AverageFileMaker(["2018", "2019"], ["01", "02"])
+    analisi = AverageFileMaker(["2022"], ["01"])
     analisi.fillDataFrames()
     # print(analisi.getYearDataFrame('2018'), "\n")
     # print(analisi.getYearDataFrame('2019'))
