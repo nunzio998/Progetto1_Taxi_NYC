a4c23900b3a067446ce13ec9b43beb163001347b
Matteo Gardusi <matteo.gardusi@gmail.com>
2022-12-18 20:47:08 +0100

    FIX minor

--- utils_app/util_mean.py
+++ utils_app/util_mean.py
@@ -7,8 +7,8 @@ import altair as alt
 
 def meanPeriodBoroughSelected(dataFrame: pd.DataFrame):
     """
-    Riceve il Dataframe dei dati graficati e restituisce il avalore medio sul periodo e sui borough selezionati
-    :param dataFrame: input daataframe
+    Riceve il Dataframe dei dati graficati e restituisce il valore medio sul periodo e sui borough selezionati:
+    :param dataFrame: input dataframe
     :return:
     """
     dfFiltered = dataFrame[~(dataFrame['borough'] != f'Boroughs Average')].reset_index(drop=True)
@@ -17,6 +17,7 @@ def meanPeriodBoroughSelected(dataFrame: pd.DataFrame):
     for date in dfFiltered['year-month']:
         numDays.append(monthrange(int(date.year), int(date.month))[1])
 
+    # calcolo media pesata
     weightedAverage = sum(numDays[g] * numTrip[g] for g in range(len(numDays))) / sum(numDays)
 
     return f"Average number of daily trips based on the selected parameters: {round(weightedAverage)}"
