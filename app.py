d63d3c2878678c5f4a5cb574030eb12bf9474c6c
Matteo Gardusi <matteo.gardusi@gmail.com>
2022-12-24 12:32:19 +0100

    ADD exception handling

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import datetime
 import pandas as pd
 import streamlit as st
-from utils_app import util_chart, util_filter, util_mean, util_barChart, util_analyses
+from utils_app import util_chart, util_filter, util_mean, util_barChart, util_analyses, util_dataset
 
 source_path = 'output/average.csv'
 
@@ -12,7 +12,7 @@ st.title('Taxi NYC')
 year = datetime.date.today().year
 years = list(range(2009, year + 1))
 years_selected = st.sidebar.multiselect(
-    'Select years', years, years[-2:], help='Years to analyze'
+    'Select years', years[::-1], years[-2:], help='Years to analyze'
 )
 
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
@@ -21,11 +21,8 @@ months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
 monthsDict = dict(zip(months, range(1, 13)))
 
 months_selected = st.sidebar.multiselect(
-    'Select months', months, months[:1], help='Months to analyze'
+    'Select months', months, months[:2], help='Months to analyze'
 )
-
-util_analyses.check_analyses(years_selected, months_selected, monthsDict)
-
 
 st.sidebar.subheader('Select Borough')
 
@@ -37,22 +34,26 @@ Queens_check = st.sidebar.checkbox('Queens', value=True)
 Staten_Island_check = st.sidebar.checkbox('Staten Island', value=True)
 Unknown_check = st.sidebar.checkbox('Unknown', value=True)
 
+if util_dataset.check_dataset(years_selected, months_selected, monthsDict):
+    util_analyses.check_analyses(years_selected, months_selected, monthsDict)
+    source = pd.read_csv(source_path, index_col=0)
 
-source = pd.read_csv(source_path, index_col=0)
+    if years_selected == [] or months_selected == []:
 
-if years_selected == [] or months_selected == []:
-
-    st.warning('Select at least one month and one year', icon="⚠️")
-else:
-    source_filtered = util_filter.filter_data(source, years_selected, months_selected, monthsDict,
-                                              Bronx_check,
-                                              Brooklyn_check,
-                                              EWR_check, Manhattan_check, Queens_check,
-                                              Staten_Island_check,
-                                              Unknown_check)
-    if Bronx_check or Brooklyn_check or EWR_check or Manhattan_check or Queens_check or Staten_Island_check or Unknown_check:
-        util_chart.line_chart(source_filtered)
-        st.subheader(util_mean.meanPeriodBoroughSelected(source_filtered))
-        util_barChart.bar_chart(source_filtered, monthsDict)
+        st.warning('Select at least one month and one year', icon="⚠️")
     else:
-        st.warning('Select at least one borough', icon="⚠️")
+        source_filtered = util_filter.filter_data(source, years_selected, months_selected, monthsDict,
+                                                  Bronx_check,
+                                                  Brooklyn_check,
+                                                  EWR_check, Manhattan_check, Queens_check,
+                                                  Staten_Island_check,
+                                                  Unknown_check)
+        if Bronx_check or Brooklyn_check or EWR_check or Manhattan_check or Queens_check or Staten_Island_check or Unknown_check:
+            util_chart.line_chart(source_filtered)
+            st.subheader(util_mean.meanPeriodBoroughSelected(source_filtered))
+            util_barChart.bar_chart(source_filtered, monthsDict)
+        else:
+            st.warning('Select at least one borough', icon="⚠️")
+
+else:
+    pass
